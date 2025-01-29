cff51c95c591b4554e01017966db18aa7152d7de
mdspenc1 <mdspenc1@ualberta.ca>
2025-01-29 15:16:47 -0700

    Delete Serial_Live_GUI directory

--- Serial_Live_GUI/configuration.py
+++
@@ -1,8 +0,0 @@
-class configuration:
-    def __init__(self, configuration_name: str, serial_variables: int, plots: int, baud_rate: int, com_port: str, csv_file: str):
-        self.configuration_name = configuration_name
-        self.serial_variables = serial_variables
-        self.plots = plots
-        self.baud_rate = baud_rate
-        self.com_port = com_port
-        self.csv_file = csv_file
