f18be832d6f2557d79086984942af0982928a34f
mdspenc1 <mdspenc1@ualberta.ca>
2025-02-06 14:17:08 -0700

    Update configuration.py

--- GUI Live Serial Reader/configuration.py
+++ GUI Live Serial Reader/configuration.py
@@ -1,3 +1,6 @@
+global configurations
+configurations = []
+
 class configuration:
     def __init__(self, configuration_name: str, serial_variables: int, plots: int, baud_rate: int, com_port: int, csv_file: str):
         self.configuration_name = configuration_name
@@ -7,5 +10,7 @@ class configuration:
         self.com_port = com_port
         self.csv_file = csv_file
 
-configurations = []
+        configurations.append(self)
 
+thermal_draw = configuration("Thermal Drawing", 5, 3, 115200, 7, "data.csv")
+test_config = configuration("meep", 6, 1, 115200, 3, "wot.csv")
