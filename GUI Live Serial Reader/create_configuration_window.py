570f3046b659c6df02953337ebbea977be050b90
mdspenc1 <mdspenc1@ualberta.ca>
2025-02-07 15:31:00 -0700

    Delete GUI Live Serial Reader directory

--- GUI Live Serial Reader/create_configuration_window.py
+++
@@ -1,9 +0,0 @@
-import customtkinter as ctk
-
-class createConfigurationWindow(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Live Serial Reader - Create New Configuration")
-        self.geometry("700x450")
-        self.resizable(True, True)
